014fda8f0adae7375bcd645225fb7b131a9e3cdf
mehdiaissa <mehdi.aissa1989@gmail.com>
2023-05-03 14:30:41 +0200

    state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
+import React from "react";
+import m1  from "react";
+import Life from "./profile/Life";
 
-import  {Form } from 'react-bootstrap';
-import './App.css';
-import Profile from './profile/Profile';
-import m1 from './profile/m1.jpg'
-import navbar from './profile/navbar';
-
-function App() {
-  const bio="Mahdi aissa is a dynamic and driven commerce professional with a passion for innovation and customer satisfaction. With a Bachelor's degree in Business Administration and several years of experience in the field, Mahdi has established himself as a respected and knowledgeable leader in the industry.After completing his degree, Mahdi started his career as a sales representative at a leading retail company, where he quickly rose through the ranks thanks to his exceptional performance and results-driven mindset. He was promoted to team leader within two years and was responsible for training and developing new sales representatives.In his current role as a commerce analyst at a top e-commerce company, Mahdi is responsible for analyzing consumer behavior and market trends, developing pricing strategies, and optimizing sales channels to maximize revenue. He is known for his ability to think outside the box and come up with innovative solutions that drive growth and improve customer satisfaction In his free time, Mahdi enjoys playing basketball and volunteering at a local nonprofit organization that helps underprivileged youth. He is also a dedicated family man, and enjoys spending time with his wife and young daughter."
-  const handleName =name =>alert(name);
-  return (
-    <div className="App">
-      <navbar/>;
-    <Profile fullName="MEHDI AISSA" bio= {bio} profession="commerciale" handleName={handleName}>
-
-       <img src={m1} alt="pic1"/>
-       </Profile> 
-     
-    </div>
-    
-  );
+class App extends React.Component {
   
+    state={fullname:"mehdi",bio:"java script",imgsrc:require("./profile/m1.jpg") ,profesion:"developer",show:false}
+    
+ 
+  render(){
+    var handelshow =()=>{
+      this.setState({show:!this.state.show})
+    }
+    return(
+      <div>
+      <button onClick={handelshow}>{this.state.show?"hide":"show"}</button>
+      {this.state.show ? 
+      <div>
+    <h1>Hello World!</h1>
+    <img src={this.state.imgsrc}/>
+    
+    </div>
+        :"clique"}
+        <Life/>
+      
+    </div>)
 
-}
-
+}}
 export default App;
