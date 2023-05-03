97b25f549d4eaee84c872b3b40cd7cb6ff88e724
mehdiaissa <mehdi.aissa1989@gmail.com>
2023-05-03 15:56:27 +0200

    mehddi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import m1  from "react";
-import Life from "./profile/Life";
-
+import Life from "./profile/Life"
 class App extends React.Component {
   
     state={fullname:"mehdi",bio:"java script",imgsrc:require("./profile/m1.jpg") ,profesion:"developer",show:false}
@@ -15,13 +14,15 @@ class App extends React.Component {
       <div>
       <button onClick={handelshow}>{this.state.show?"hide":"show"}</button>
       {this.state.show ? 
-      <div>
-    <h1>Hello World!</h1>
+      <div className="Profil"> 
+    <h1>{this.state.fullname}</h1>
+    <h3>{this.state.bio}</h3>
     <img src={this.state.imgsrc}/>
+    <Life/>
     
     </div>
         :"clique"}
-        <Life/>
+        
       
     </div>)
 
